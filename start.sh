62811f6ec2e58fac33c5d5c82cd8e33456250350
Veeramalla Sai <veeramallasaipichaiah456@gmail.com>
2026-08-14 16:53:44 +0530

    Fix Flutter Railway start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/sh
+set -e
+
+echo "Installing Flutter..."
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable /opt/flutter
+export PATH="/opt/flutter/bin:$PATH"
+
+flutter --version
+flutter pub get
+flutter build web --release --no-wasm-dry-run
+
+cd build/web
+python3 -m http.server "${PORT:-8080}" --bind 0.0.0.0
